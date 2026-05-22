a3b322e8bd1ad329a6fb39d80a9845ccbb037d04
Eltun2006 <seltun.memmedli.06@gmail.com>
2026-05-22 07:18:19 +0000

    Add 0-hbtn_status.py

---
+++ basics_1/python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
